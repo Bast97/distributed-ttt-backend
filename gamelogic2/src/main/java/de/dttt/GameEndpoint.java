39c5c1cbafda1d583c56151bc3b67ea07a44517e
Bast97 <bastian.schetter@unitybox.de>
2021-06-29 18:15:51 +0200

    Made a fixed reaction to turn

--- gamelogic2/src/main/java/de/dttt/GameEndpoint.java
+++ gamelogic2/src/main/java/de/dttt/GameEndpoint.java
@@ -3,7 +3,9 @@ package de.dttt;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 import javax.websocket.EncodeException;
@@ -14,33 +16,48 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import com.google.gson.Gson;
+
+import de.dttt.beans.WSBean;
+import de.dttt.beans.WSGameState;
+import de.dttt.beans.WSTurn;
 
 @ServerEndpoint(value = "/{gameID}")
 public class GameEndpoint {
 	private Session session;
-	private static final Set<GameEndpoint> connections = new CopyOnWriteArraySet<>();
-	private static HashMap<String, String> users = new HashMap<>(); // TODO:: Users are never removed
+	private static Map<String, Session> endpoints = new ConcurrentHashMap<>();
+	private static Gson gson = new Gson();
 
 	@OnOpen
 	public void onOpen(Session session, @PathParam("gameID") String gameID) throws IOException, EncodeException {
 
 		this.session = session;
-		connections.add(this);
+		if(endpoints.get(session.getId()) == null) {
+			endpoints.put(session.getId(), session);
+		}
 
 		System.out.println("\nSocket " + session.getId() + " connected to endpoint " + gameID);
-
+		
 	}
 
 	@OnMessage
 	public void onMessage(Session session, @PathParam("gameID") String gameID, String data)
 			throws IOException, EncodeException {
 		System.out.println(data);
-		
+		WSBean bean = gson.fromJson(data, WSBean.class);
+		switch(bean.getType()) {
+			case WSBean.TURN:
+				System.out.println("Received TURN message");
+				handlerTurn(session, bean.getData());
+				break;
+			default:
+				System.out.println("Unknown type of message");
+		}
 	}
 
 	@OnClose
 	public void onClose(Session session) throws IOException, EncodeException {
-		connections.remove(this);
+		this.endpoints.remove(session.getId());
 		System.out.println("Socket disconnected: " + session.getId());
 	}
 
@@ -49,23 +66,26 @@ public class GameEndpoint {
 		// Do error handling here
 	}
 
-	private void broadcast(TTTMatch message) throws IOException, EncodeException {
+	private void handlerTurn(Session session, String data) {
+		WSTurn turnBean = gson.fromJson(data, WSTurn.class);
+		System.out.println(turnBean.getX());
+		System.out.println(turnBean.getY());
 
-		ArrayList<String> sessions = new ArrayList<>();
-		sessions.add(users.get(this.matchState.getUserX()));
-		sessions.add(users.get(this.matchState.getUserO()));
+		// Überprüfe Daten
 
-		connections.forEach(endpoint -> {
-			synchronized (endpoint) {
-				if (sessions.contains(endpoint.session.getId())) {
-					try {
-						endpoint.session.getBasicRemote().sendObject(message);
-					} catch (IOException | EncodeException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
+		// Schreibe Match Informationen
+
+		// Regiere dann auf die Änderungen an der REDIS Datenbank
+
+		// TESTWEISE:
+
+		int[] gamestate = {1,1,2,0,0,0,0,0,0};
+		WSGameState dataBean = new WSGameState(gamestate);
+		try {
+			session.getBasicRemote().sendText(new WSBean(WSBean.TURN, dataBean).toJson());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
