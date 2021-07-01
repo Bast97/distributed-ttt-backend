a38ed346fce9dffdbdf672840ecceb5e6c12a4c6
Johannes Hirth <hirth.johannes@gmx.de>
2021-07-01 16:21:21 +0200

    gamelogic making matchmaker API request

--- gamelogic/src/main/java/de/dttt/GameEndpoint.java
+++ gamelogic/src/main/java/de/dttt/GameEndpoint.java
@@ -1,6 +1,10 @@
 package de.dttt;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
@@ -36,6 +40,24 @@ public class GameEndpoint {
 			this.matchState = games.get(gameID);
 			broadcast(matchState);
 		} else if (true) { // TODO: Call Matchmaker for Match Details here
+			
+			URL url = new URL("http://localhost:8080/matchinfo/" + gameID);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			int status = con.getResponseCode();
+			System.out.println("HTTP Response Status: " + Integer.toString(status));
+			BufferedReader in = new BufferedReader(
+				new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				StringBuffer content = new StringBuffer();
+				while ((inputLine = in.readLine()) != null) {
+					content.append(inputLine);
+				}
+			in.close();
+			con.disconnect();
+			System.out.println("content: " + content);
+
+
 			TTTMatch newMatch = new TTTMatch(gameID, "123", "456");
 			games.put(gameID, newMatch);
 			this.matchState = newMatch;
