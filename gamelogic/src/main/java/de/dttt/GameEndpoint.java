3d3cd9bc4bc00d9d4028e0106e0640a5f6862b73
Johannes Hirth <hirth.johannes@gmx.de>
2021-07-03 16:47:43 +0200

    now working with data management solely on Redis db

--- gamelogic/src/main/java/de/dttt/GameEndpoint.java
+++ gamelogic/src/main/java/de/dttt/GameEndpoint.java
@@ -15,33 +15,20 @@ import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 import com.google.gson.Gson;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import de.dttt.redis.RedisConfig;
-import de.dttt.redis.MessagePublisher;
-import de.dttt.redis.RedisMessagePublisher;
-import de.dttt.redis.repo.MatchRepository;
 import redis.clients.jedis.Jedis;
-import de.dttt.redis.Match;
 import de.dttt.beans.WSBean;
 import de.dttt.beans.WSTurn;
 
 @ServerEndpoint(value = "/{gameID}", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
-@ContextConfiguration(classes = RedisConfig.class)
 public class GameEndpoint {
 	private Session session;
-	private TTTMatch matchState;
 	private static final Set<GameEndpoint> connections = new CopyOnWriteArraySet<>();
-	private static HashMap<String, TTTMatch> games = new HashMap<>();
 	
 	Jedis jedis = new Jedis("35.204.146.65", 6379);
 	Gson gson = new Gson();
 	
-    @Autowired(required = true)
-    private MatchRepository matchRepository;
-    @Autowired
-    private MessagePublisher redisMessagePublisher;
+    Jedis mjedis = null;
+	
 
 	@OnOpen
 	public void onOpen(Session session, @PathParam("gameID") String gameID) throws IOException, EncodeException {
@@ -53,23 +40,19 @@ public class GameEndpoint {
 
 		MatchmakerInfo mmInfo = new MatchmakerInfo(gameID);
 
-		if (games.containsKey(gameID)) { // I think this runs when player 2 joins
-			this.matchState = games.get(gameID);
-			matchState.setUserO(mmInfo.getO());
+
+
+		if (jedis.exists(gameID)) { // I think this runs when player 2 joins
 			String json = jedis.get(gameID);
 			TTTMatch match = gson.fromJson(json, TTTMatch.class);
 			match.setUserO(mmInfo.getO());
 			json = gson.toJson(match);
 			jedis.set(gameID, json);
-
 			
 			broadcast(match);
-			broadcast(matchState);
+			// broadcast(matchState);
 		} else if (mmInfo.isValid()) { // I think this runs when player 1 joins
-			TTTMatch newMatch = new TTTMatch(gameID, mmInfo.getX());
-			games.put(gameID, newMatch);
-			this.matchState = newMatch;
-			
+			TTTMatch newMatch = new TTTMatch(gameID, mmInfo.getX());			
 			String json = gson.toJson(newMatch);
 			jedis.set(gameID, json);
 			System.out.println("New Match was created");
@@ -111,7 +94,6 @@ public class GameEndpoint {
 					} finally {
 						System.out.println("Game over! Removing...");
 					}
-					games.remove(gameID);
 				}
 			} else {
 				System.out.println("Move was illegal! State was not updated.");
@@ -120,39 +102,6 @@ public class GameEndpoint {
 
 		}
 
-		if (matchState.nextTurn(turn)) {
-
-			// int[] gamestate = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-			// int gamestate = 1;
-			// System.out.println("about to save");
-			// Match match = new Match(gameID, "player1id", "player2id", gamestate);
-			// String json = gson.toJson(match);
-
-
-
-
-			// try {
-			// 	jedis.set(gameID, json);
-			// 	jedis.close();
-			// 	matchRepository.save(match);
-			// 	System.out.println("saved succesfully");
-			// } catch (Exception e) {
-			// 	System.out.println(e.toString());
-			// }
-
-			broadcast(matchState);
-			System.out.println("Move was legal. State updated.");
-			if (matchState.isOver()) {
-				try {
-					System.out.println(matchState.getWinnerUID() + " won!");
-				} finally {
-					System.out.println("Game over! Removing...");
-				}
-				games.remove(gameID);
-			}
-		} else {
-			System.out.println("Move was illegal! State was not updated.");
-		}
 	}
 
 	@OnClose
@@ -168,16 +117,18 @@ public class GameEndpoint {
 
 	private void broadcast(TTTMatch message) {
 
+		System.out.println("broadcast to: ");
 		connections.forEach(endpoint -> {
+			System.out.println(endpoint);
 			synchronized (endpoint) {
-				if (endpoint.matchState == message) {
+				// if (endpoint.matchState == message) {
 					try {
 						endpoint.session.getBasicRemote()
 								.sendObject(new WSBean(WSBean.TURN, message.toGameState()));
 					} catch (IOException | EncodeException e) {
 						e.printStackTrace();
 					}
-				}
+				// }
 			}
 		});
 	}
--- gamelogic/src/main/java/de/dttt/redis/MessagePublisher.java
+++
@@ -1,6 +0,0 @@
-package de.dttt.redis;
-
-public interface MessagePublisher {
-
-    void publish(final String message);
-}
