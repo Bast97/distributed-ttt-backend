9723b626c1382b21507a6d83021cbd9de5305c76
Johannes Hirth <hirth.johannes@gmx.de>
2021-08-03 00:16:03 +0200

    matchmaker now maybe scalable

--- matchmaker/src/main/java/io/swagger/matchmaker/Matchmaker.java
+++ matchmaker/src/main/java/io/swagger/matchmaker/Matchmaker.java
@@ -7,44 +7,137 @@ import io.swagger.matchmaker.Match;
 import java.util.Date;
 import java.util.UUID;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
 public class Matchmaker {
 
-    static ArrayList<Match> matches = new ArrayList<Match>();
+	JedisPoolConfig config;
+	static JedisPool jedisPool;
+	Gson gson = new Gson();
+	String redis_host = null;
+	String redis_port = null;
+	private static final String JEDIS_PASSWORD = System.getenv("REDIS_PASSWORD");
     
     public Matchmaker() {
+		redis_host = System.getenv("REDIS_HOST");
+		redis_port = System.getenv("REDIS_PORT");
+		config = new JedisPoolConfig();
+		// JedisPool config
+		config.setMaxTotal(10000); // Set the maximum number of connections
+		config.setMaxIdle(10000); // Set the maximum number of idle connections
+
+		jedisPool = new JedisPool(config, redis_host, Integer.parseInt(redis_port));
+
+        
+        Jedis jedis = jedisPool.getResource();
+		jedis.auth(JEDIS_PASSWORD);
+        try {
+            if(!jedis.exists("matchlist")) {
+                ArrayList<Match> matches = new ArrayList<Match>();
+                String json = gson.toJson(matches);
+				jedis.set("matchlist", json);
+            }
+        } finally {
+			if(jedis != null)
+                jedis.close();
+        }
     }
 
-    public void addMatch(Match match) {
-        matches.add(match);
+    public void addMatch(Match match) {        
+        Jedis jedis = jedisPool.getResource();
+		jedis.auth(JEDIS_PASSWORD);
+        try {
+            if(jedis.exists("matchlist")) {
+                String json = jedis.get("matchlist");
+                ArrayList<Match> matches = gson.fromJson(json, new TypeToken<ArrayList<Match>>(){}.getType());
+                matches.add(match);
+                
+                json = gson.toJson(matches);
+				jedis.set("matchlist", json);
+            }
+        } finally {
+			if(jedis != null)
+                jedis.close();
+        }
     }
 
     public int getNumberOfMatches() {
-        return matches.size();
+        int size = 0;
+        Jedis jedis = jedisPool.getResource();
+		jedis.auth(JEDIS_PASSWORD);
+        try {
+            if(jedis.exists("matchlist")) {
+                String json = jedis.get("matchlist");
+                ArrayList<Match> matches = gson.fromJson(json, new TypeToken<ArrayList<Match>>(){}.getType());
+                size = matches.size();
+            }
+        } finally {
+			if(jedis != null)
+                jedis.close();
+        }
+        return size;
     }
 
     public void deleteMatch(String matchId) {
-        for (int i = 0; i < matches.size(); i++)  {
-            if(matches.get(i).getMatchId().equals(matchId)) {
-                matches.remove(i);
-                return;
+        Jedis jedis = jedisPool.getResource();
+		jedis.auth(JEDIS_PASSWORD);
+        try {
+            if(jedis.exists("matchlist")) {
+                String json = jedis.get("matchlist");
+                ArrayList<Match> matches = gson.fromJson(json, new TypeToken<ArrayList<Match>>(){}.getType());
+                
+                for (int i = 0; i < matches.size(); i++)  {
+                    if(matches.get(i).getMatchId().equals(matchId)) {
+                        matches.remove(i);
+                        json = gson.toJson(matches);
+                        jedis.set("matchlist", json);
+                        jedis.close();
+                        return;
+                    }
+                }
             }
+        } finally {
+			if(jedis != null)
+                jedis.close();
         }
     }
 
     public NewMatch joinMatch(String userId) {
-        for (int i = 0; i < matches.size(); i++)  {
-            if (matches.get(i).getPlayerCount() == 1) {
-                Match match = matches.get(i);
-                match.setPlayerCount(2);
-                match.setPlayer2Id(userId);
+        Jedis jedis = jedisPool.getResource();
+		jedis.auth(JEDIS_PASSWORD);
+        String json = jedis.get("matchlist");
+        ArrayList<Match> matches = gson.fromJson(json, new TypeToken<ArrayList<Match>>(){}.getType());
 
-                NewMatch newMatch = new NewMatch();
-                newMatch.setMatchId(match.getMatchId());
-                newMatch.setPlayerId(userId);
-                newMatch.setPlayerNum(2);
+        try {
+            if(jedis.exists("matchlist")) {
+                for (int i = 0; i < matches.size(); i++)  {
+                    if (matches.get(i).getPlayerCount() == 1) {
+                        Match match = matches.get(i);
+                        match.setPlayerCount(2);
+                        match.setPlayer2Id(userId);
 
-                return newMatch;
+                        matches.set(i, match);
+                        json = gson.toJson(matches);
+                        jedis.set("matchlist", json);
+
+                        NewMatch newMatch = new NewMatch();
+                        newMatch.setMatchId(match.getMatchId());
+                        newMatch.setPlayerId(userId);
+                        newMatch.setPlayerNum(2);
+
+                        jedis.close();
+                        return newMatch;
+                    }
+                }
             }
+        } finally {
+			if(jedis != null)
+                jedis.close();
         }
 
         String matchId = "m" + UUID.randomUUID().toString();
@@ -67,15 +160,29 @@ public class Matchmaker {
 
     public MatchInfo getMatchInfoForMatchId(String matchId) {
         
-        for (int i = 0; i < matches.size(); i++)  {
-            if(matches.get(i).getMatchId().equals(matchId)) {
-                MatchInfo matchInfo = new MatchInfo();
-                matchInfo.setMatchIdIsValid(true);
-                matchInfo.setPlayer1Id(matches.get(i).getPlayer1Id());
-                matchInfo.setPlayer2Id(matches.get(i).getPlayer2Id());
-                return matchInfo;
+        Jedis jedis = jedisPool.getResource();
+		jedis.auth(JEDIS_PASSWORD);
+        String json = jedis.get("matchlist");
+        ArrayList<Match> matches = gson.fromJson(json, new TypeToken<ArrayList<Match>>(){}.getType());
+
+        try {
+            if(jedis.exists("matchlist")) {
+                for (int i = 0; i < matches.size(); i++)  {
+                    if(matches.get(i).getMatchId().equals(matchId)) {
+                        MatchInfo matchInfo = new MatchInfo();
+                        matchInfo.setMatchIdIsValid(true);
+                        matchInfo.setPlayer1Id(matches.get(i).getPlayer1Id());
+                        matchInfo.setPlayer2Id(matches.get(i).getPlayer2Id());
+                        jedis.close();
+                        return matchInfo;
+                    }
+                }
             }
+        } finally {
+			if(jedis != null)
+                jedis.close();
         }
+
         MatchInfo matchInfo = new MatchInfo();
         matchInfo.setMatchIdIsValid(false);
         return matchInfo;
